dd6ea8f03c2e88749b2819f4d2d96c4a33b3d85a
Patrick Neal <pat.neal@rocketinsights.com>
2021-07-07 17:13:23 -0400

    Fixed linting issue

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,13 +3,16 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import logo from '../../logo.png'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    headerLogo: {
+      width: '24px',
+      transform: 'scale(1.25)'
+    },
     root: {
       flexGrow: 1,
     },
@@ -27,7 +30,11 @@ function Header() {
       <AppBar position="fixed">
         <Toolbar>
           <IconButton edge="start" color="inherit" aria-label="menu">
-            <img style={{'width': '24px', 'transform': 'scale(1.25)'}} src={logo} />
+            <img
+              alt="Pokémon Compster Logo"
+              className={classes.headerLogo}
+              src={logo}
+            />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Pokémon Compster
